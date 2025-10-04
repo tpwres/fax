1856fdf3ecc90adb8a825e57468d6de4c63c14ae
Krzysztof Zych <k33rni@gmail.com>
2025-10-04 16:36:42 +0200

    Handmade auth in progress

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { Hono } from 'hono'
+
 const app = new Hono()
 
+const GITHUB_AUTH_ENDPOINT = 'https://github.com/login/auth/authorize'
+
 app.get('/auth/login', (c) => {
-  return c.text(`Hello worker ${c.req.url}`)
+  const github = new URL(GITHUB_AUTH_ENDPOINT)
+  
+  // TODO: Replace with BetterAuth?
+  const auth_client_id = process.env.OAUTH_CLIENT_ID as string
+  const auth_callback = process.env.AUTH_CALLBACK_URL as string
+  
+  const params = github.searchParams
+  params.set('client_id', auth_client_id)
+  params.set('redirect_uri', auth_callback)
+  params.set('scope', 'read:user')
+  return c.redirect(github)
+})
+
+app.get('/auth/callback', (c) => {
+	const client_id = c.req.param('client_id')
+	const client_secret = c.req.param('client_secret')
+	const code = c.req.param('code')
+
+	return c.text(`
+		clientid=${client_id}
+		secret=${client_secret}
+		code=${code}
+	`)
 })
 
 export default {
